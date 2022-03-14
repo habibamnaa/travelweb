388fb576398f0cc894fa9ba42420fd8b3b2f084d
Habib Amna <habibamna14@gmail.com>
2022-03-14 15:15:11 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "assets/scss/style.scss";
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import LandingPage from "pages/LandingPage";
+
+
+function App() {
+    return <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+}
+
+export default App;
